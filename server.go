e079b33fa7ea15f417d1b58f72ca0a7ce683b1d0
Abdelrahman Maged <abdulrahman.maged95@gmail.com>
2016-12-14 00:06:50 +0200

    A more convulated GraphQL setup

--- server.go
+++ server.go
@@ -4,17 +4,82 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"github.com/graphql-go/graphql"
 	"net/http"
 	"os"
 	"strings"
 )
 
+type user struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+var data map[string]user
+
+var userType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "User",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.String,
+			},
+			"name": &graphql.Field{
+				Type: graphql.String,
+			},
+		},
+	},
+)
+
+var queryType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Query",
+		Fields: graphql.Fields{
+			"user": &graphql.Field{
+				Type: userType,
+				Args: graphql.FieldConfigArgument{
+					"id": &graphql.ArgumentConfig{
+						Type: graphql.String,
+					},
+				},
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					idQuery, isOK := p.Args["id"].(string)
+					if isOK {
+						return data[idQuery], nil
+					}
+					return nil, nil
+				},
+			},
+		},
+	})
+
+var schema, _ = graphql.NewSchema(
+	graphql.SchemaConfig{
+		Query: queryType,
+	},
+)
+
+func executeQuery(query string, schema graphql.Schema) *graphql.Result {
+	result := graphql.Do(graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+	})
+	if len(result.Errors) > 0 {
+		fmt.Printf("Wrong result, unexpected errors: %v", result.Errors)
+	}
+	return result
+}
+
 func main() {
+	data = make(map[string]user)
+	data["1"] = user{"1", "Dan"}
+
 	http.HandleFunc("/api/login", loginHandler)
 	http.HandleFunc("/api/coursework", courseworkHandler)
 	http.HandleFunc("/api/midterms", midtermsHandler)
 	http.HandleFunc("/api/attendance", attendanceHandler)
 	http.HandleFunc("/api/exams", examsHandler)
+	http.HandleFunc("/graphql", graphqlHandler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -63,6 +128,11 @@ func examsHandler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		sendJsonResponse(w, ResponseAPI{nil, exams})
 	}
+}
+
+func graphqlHandler(w http.ResponseWriter, r *http.Request) {
+	result := executeQuery(r.URL.Query()["query"][0], schema)
+	sendJsonResponse(w, result)
 }
 
 func basicAuthentication(r *http.Request) (string, string) {
