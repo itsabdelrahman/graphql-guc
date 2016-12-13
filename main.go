3e80297c98d39cfb8358522c634b4a09de2bf93c
Abdelrahman Maged <abdulrahman.maged95@gmail.com>
2016-12-13 23:09:02 +0200

    Format code

--- main.go
+++ main.go
@@ -17,18 +17,22 @@ func main() {
 			},
 		},
 	}
+
 	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
 	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
 	schema, _ := graphql.NewSchema(schemaConfig)
 
 	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
 		query := r.URL.Query()["query"][0]
+
 		result := graphql.Do(graphql.Params{
 			Schema: schema,
 			RequestString: query,
 		})
+
 		json.NewEncoder(w).Encode(result)
 	})
+	
 	fmt.Println("Now server is running on port 8080...")
 	fmt.Println("Test with GET: curl -g 'http://localhost:8080/graphql?query={hero{name}}'")
 	http.ListenAndServe(":8080", nil)
