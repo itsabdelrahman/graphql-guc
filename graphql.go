4292eab548fec3c9247ba762ec539df56332bfc5
Abdelrahman Maged <abdulrahman.maged95@gmail.com>
2016-12-14 18:47:38 +0200

    Add midterms to party

--- graphql.go
+++ graphql.go
@@ -51,12 +51,12 @@ var (
 				"coursework": &graphql.Field{
 					Type: graphql.NewList(courseworkType),
 					Args: graphql.FieldConfigArgument{
-						"name": &graphql.ArgumentConfig{
+						"course": &graphql.ArgumentConfig{
 							Type: graphql.String,
 						},
 					},
 					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-						courseName, isCourseNameOK := p.Args["name"].(string)
+						courseName, isCourseNameOK := p.Args["course"].(string)
 
 						student := p.Source.(StudentAPI)
 						allCoursework, _ := GetUserCoursework(student.Username, student.Password)
@@ -73,7 +73,28 @@ var (
 					},
 				},
 				"midtermsGrades": &graphql.Field{
-					Type: graphql.NewList(graphql.String),
+					Type: graphql.NewList(midtermType),
+					Args: graphql.FieldConfigArgument{
+						"course": &graphql.ArgumentConfig{
+							Type: graphql.String,
+						},
+					},
+					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+						courseName, isCourseNameOK := p.Args["course"].(string)
+
+						student := p.Source.(StudentAPI)
+						allMidterms, _ := GetUserMidterms(student.Username, student.Password)
+
+						if isCourseNameOK {
+							for _, midterm := range allMidterms {
+								if strings.Contains(midterm.Name, courseName) {
+									return []MidtermAPI{midterm}, nil
+								}
+							}
+						}
+
+						return allMidterms, nil
+					},
 				},
 				"absenceLevels": &graphql.Field{
 					Type: graphql.NewList(graphql.String),
@@ -110,6 +131,20 @@ var (
 					Type: graphql.Float,
 				},
 				"maxPoint": &graphql.Field{
+					Type: graphql.Float,
+				},
+			},
+		},
+	)
+
+	midtermType = graphql.NewObject(
+		graphql.ObjectConfig{
+			Name: "midtermsGrades",
+			Fields: graphql.Fields{
+				"course": &graphql.Field{
+					Type: graphql.String,
+				},
+				"percentage": &graphql.Field{
 					Type: graphql.Float,
 				},
 			},
