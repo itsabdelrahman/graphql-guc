206dceccb910a9c2005d20c80f2c22feac0e0858
A.R. Maged <abdulrahman.maged95@gmail.com>
2017-12-11 23:22:11 +0200

    :tada: Launch server

---
+++ src/app.js
@@ -0,0 +1,37 @@
+import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
+// import { makeExecutableSchema } from 'graphql-tools';
+// import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+// import { graphqlSchema, graphqlResolvers } from './graphql';
+// import config from './constants/config';
+
+const app = express();
+app.server = http.createServer(app);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// const executableSchema = makeExecutableSchema({
+//   typeDefs: [graphqlSchema],
+//   resolvers: graphqlResolvers,
+// });
+
+// app.use(
+//   '/graphql',
+//   bodyParser.json(),
+//   graphqlExpress({
+//     schema: executableSchema,
+//   }),
+// );
+
+// app.use(
+//   '/graphiql',
+//   graphiqlExpress({
+//     endpointURL: '/graphql',
+//   }),
+// );
+
+// app.server.listen(process.env.PORT || config.server.port);
+app.server.listen(process.env.PORT || 8080);
+console.log(`🚀  Server listening on port ${app.server.address().port}...`);
