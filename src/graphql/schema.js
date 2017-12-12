d840540ab72ea19e7169d492f2f95c11f7fadaa1
A.R. Maged <abdulrahman.maged95@gmail.com>
2017-12-12 15:25:56 +0200

    :recycle: Update GraphQL schema

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -1,13 +1,72 @@
 export default `
-type Student {
-  isAuthorized: Boolean
+schema {
+  query: Query
 }
 
 type Query {
   student(email: String!, password: String!): Student
 }
 
-schema {
-  query: Query
+type Student {
+  courses: [Course]
+  schedule: [Slot]
+}
+
+type Course {
+  code: String
+  name: String
+  absence: Absence
+  coursework: [Component]
+  midterm: Midterm
+  exam: Exam
+}
+
+type Absence {
+  level: Int
+  severity: AbsenceSeverity
+}
+
+type Slot {
+  type: SlotType
+  weekday: SlotWeekday
+  number: Int
+  venue: String
+}
+
+type Component {
+  type: String
+  grade: Float
+  maximumGrade: Float
+}
+
+type Midterm {
+  grade: Float
+}
+
+type Exam {
+  venue: String
+  seat: String
+  startsAt: String
+}
+
+enum SlotWeekday {
+  SATURDAY
+  SUNDAY
+  MONDAY
+  TUESDAY
+  WEDNESDAY
+  THURSDAY
+}
+
+enum SlotType {
+  LECTURE
+  TUTORIAL
+  LAB
+}
+
+enum AbsenceSeverity {
+  HIGH
+  MEDIUM
+  LOW
 }
 `;
