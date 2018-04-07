897e761d38e04f063a31cdf0fb3528df7949ae12
A.R. Maged <abdulrahman.maged95@gmail.com>
2018-04-07 22:20:41 +0200

    :white_check_mark: Test AES encryption & decryption functions

--- src/__tests__/utilities.test.js
+++ src/__tests__/utilities.test.js
@@ -1,7 +1,23 @@
-import { capitalize } from '../utilities';
+import { capitalize, encrypt, decrypt } from '../utilities';
 
 describe('capitalize()', () => {
   test('It should capitalize the first character in a string', () => {
     expect(capitalize('quiz')).toBe('Quiz');
+  });
+});
+
+describe('encrypt()', () => {
+  test('It should encrypt a plain string', () => {
+    expect(encrypt('secret_key')('secret_data')).toBe(
+      'b40834e6b6bd202b05c47d9a0adf3b08',
+    );
+  });
+});
+
+describe('decrypt()', () => {
+  test('It should decrypt an encrypted string', () => {
+    expect(decrypt('secret_key')('b40834e6b6bd202b05c47d9a0adf3b08')).toBe(
+      'secret_data',
+    );
   });
 });
